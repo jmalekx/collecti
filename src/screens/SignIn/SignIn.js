8c8a13bdd42dd7ed8b2d43d468fea00b96948b2d
jmalekx <151661788+jmalekx@users.noreply.github.com>
2025-01-26 18:31:52 +0000

    auth

--- src/screens/SignIn/SignIn.js
+++ src/screens/SignIn/SignIn.js
@@ -1,19 +1,60 @@
 import React, { useState } from 'react';
-import { View, Text, Image, StyleSheet, useWindowDimensions} from 'react-native';
+import { View, Text, Button, TextInput, Image, StyleSheet, useWindowDimensions, ActivityIndicator, KeyboardAvoidingView} from 'react-native';
 import { FIREBASE_AUTH } from '../../../FirebaseConfig';
 import Logo from '../../../assets/images/tmplogo.png';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 const SignIn = () => {
     const {height} = useWindowDimensions();
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false);
     const auth = FIREBASE_AUTH;
+
+    const logIn = async () => {
+        setLoading(true);
+        try {
+            const repsonse = await signInWithEmailAndPassword(auth, email, password);
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+            alert('login failed');
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const signUp = async () => {
+        setLoading(true);
+        try {
+            const repsonse = await createUserWithEmailAndPassword(auth, email, password);
+            console.log(response);
+            alert('check mail');
+        } catch (error) {
+            console.log(error);
+            alert('signup failed');
+        } finally {
+            setLoading(false);
+        }
+    }
     
     return (
         <View style={styles.root}>
             <Image source = {Logo} style={[styles.logo, {height: height * 0.45}]} resizeMode='contain'/>
-            <Text> Sign in screen </Text>
+            <KeyboardAvoidingView behavior='padding'>
+                <TextInput value={email} style={styles.input} placeholder="Email" autoCapitalize="none" onChangeText={(text) => setEmail(text)}></TextInput>
+                <TextInput secureTextEntry={true} value={password} style={styles.input} placeholder="password" autoCapitalize="none" onChangeText={(text) => setPassword(text)}></TextInput>
+            
+            { loading ? (
+                <ActivityIndicator size="large" color="#0000ff"/>
+            ) : (
+                <>
+                    <Button title="Login" onPress={() => logIn()}/>
+                    <Button title="Sign Up" onPress={() => signUp()}/>
+                </>
+            )}
+            </KeyboardAvoidingView>
         </View>
     );
 };
