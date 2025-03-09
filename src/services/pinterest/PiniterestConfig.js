57a5c06aa724a494697023bf4c05a9a2ac94af66
jmalekx <151661788+jmalekx@users.noreply.github.com>
2025-03-09 15:14:06 +0000

    config

--- src/services/pinterest/PiniterestConfig.js
+++
@@ -1,6 +0,0 @@
-export const PINTEREST_CONFIG = {
-    CLIENT_ID: '1507461',
-    CLIENT_SECRET: 'e33c5528a1ccf8fe350fb6d0e440bc0f93ff66f4',
-    REDIRECT_URI: 'collecti://oauth',
-    SCOPE: 'boards:read,pins:read,user_accounts:read',
-  };
