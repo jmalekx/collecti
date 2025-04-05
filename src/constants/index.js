31c4548678a589eb208ad1924ad0dff56dd3ec30
jmalekx <151661788+jmalekx@users.noreply.github.com>
2025-04-05 19:09:27 +0100

    Default colleciton thumbnail

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,8 @@
+import { Asset } from 'expo-asset';
+
 export const DEFAULT_PROFILE_PICTURE =
   'https://i.pinimg.com/736x/9c/8b/20/9c8b201fbac282d91c766e250d0e3bc6.jpg';
 
-export const DEFAULT_THUMBNAIL =
-  'https://i.pinimg.com/736x/f6/51/5a/f6515a3403f175ed9a0df4625daaaffd.jpg';
+// Get the URI of the local asset
+const defaultThumbnailAsset = Asset.fromModule(require('../images/thumbnail.png'));
+export const DEFAULT_THUMBNAIL = defaultThumbnailAsset.uri;
